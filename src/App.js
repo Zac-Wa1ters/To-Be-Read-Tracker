94e1606553aa67ac38d70487fedf3a020a28ecdb
Your Name <efondren@basecampcodingacademy.org>
2025-10-20 17:32:00 -0500

    Evalyn first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import BookForm from "./components/BookForm";
+import BookList from "./components/BookList";
+import CompletedList from "./components/CompletedList";
+import { getBooks, addBook, updateBook, deleteBook } from "./api";
+import ToBeReadTracker from './ToBeReadTracker.jsx';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return (<>ToBeReadTracker</>)
 }
 
 export default App;
